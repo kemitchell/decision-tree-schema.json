ad585a8878c9f30128a2346d92690ead8cf567b7
Kyle Mitchell <kyle@kemitchell.com>
2015-12-24 17:08:15 -0800

    Build exported schema with json-schema-ref-parser

---
+++ build.js
@@ -0,0 +1,8 @@
+require('json-schema-ref-parser').bundle(
+  require('./schema.json'),
+  function(error, schema) {
+    if (error) {
+      console.error(error)
+      process.exit(1) }
+    else {
+      console.log('%j', schema) } })
